cb91f44c501acc6ab5975bace2baa8e6ce98e1f8
Fernando Yedra <A2183920@correo.uia.mx>
2021-02-24 16:29:34 -0600

    Arreglo al main

--- pacientes.c
+++ pacientes.c
@@ -6,34 +6,33 @@
  */
 
 #include "defPacientes.h"
-//Incluir prototipos
+//TODO Incluir prototipos
 
 int main(int argc, char *argv[]){
 	char opcion;
 	info alumno;
 	nodo *inicio=NULL;
 	FILE *fp;
-
-	//TODO Cambiar switch por if
+	printf("%i\n",strcmp(argv[1],"-b"));
 	printf("-----Catálogo de alumnos------\n");
-	switch(argv[1][0]){
-		case 't':
-			printf("Leyendo archivo %s\n", argv[2]);
-			fp=fopen(argv[2], "r");
-			if(fp==NULL){
-				printf("Archivo no disponible\n");
-				exit(1);
-			}
-			while(fscanf(fp, " %[^\t] %[^\t] %i %c\n", alumno.nombre, alumno.direccion, &alumno.telefono, &alumno.covid)==4){
-				printf("leer\n");//TODO Solo para verificar que pase
-				//TODO Función para anexar nodo (alumno, inicio)
-			}
-			fclose(fp);
-			break;
-		case 'b':
-			printf("Recuperando catálogo guardado\n");
-			//TODO Función para recuperar delbinario (puede ser la misma)
-			break;
+	if(strcmp(argv[1],"-h")==1){
+		printf("Leyendo archivo %s\n", argv[2]);
+		fp=fopen(argv[2], "r");
+		if(fp==NULL){
+			printf("Archivo no disponible\n");
+			exit(1);
+		}
+		while(fscanf(fp, " %[^\t] %[^\t] %i %c\n", alumno.nombre, alumno.direccion, &alumno.telefono, &alumno.covid)==4){
+			printf("leer\n");//TODO Solo para verificar que pase
+			//TODO Función para anexar nodo (alumno, inicio)
+		}
+		fclose(fp);
+	}else if(strcmp(argv[1],"-b")==1){
+		printf("Recuperando catálogo guardado\n");
+		//TODO Función para recuperar delbinario (puede ser la misma)
+	}else{
+		printf("Selección de comando inválida, salienod del programa\n");
+		return 1;
 	}
 	while(opcion!='s'){
 		printf("---MENU---\n");
@@ -66,6 +65,6 @@ int main(int argc, char *argv[]){
 				break;
 		}
 	}
-	
+	//TODO Función para guardar el arvhivo binario
 	return 0;
 }
